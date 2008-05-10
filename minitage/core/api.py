cf03f0962a66bc4693baa124fef00cd268ad3d58
kiorky <devnull@localhost>
2008-05-10 15:49:12 +0200

    new functions from module interfaces

--- minitage/core/api.py
+++ minitage/core/api.py
@@ -16,4 +16,6 @@ __docformat__ = 'restructuredtext en'
 from minitage.core.collections import LazyLoadedList
 from minitage.core.objects import Minilay, Minibuild
 from minitage.core.core import Minimerge
+from minitage.makers.interfaces import IMakerFactory
+from minitage.fetchers.interfaces import IFetcherFactory
 
