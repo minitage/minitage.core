8ca9c6873df4b3be187c6837d647b73555dc16da
makina <makina@APEM3.(none)>
2009-11-30 11:49:29 +0100

    win32port

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ import sys
 from setuptools import setup, find_packages
 
 name = 'minitage.core'
-version = '1.0.38'
+version = '1.0.39'
 def read(rnames):
     setupdir =  os.path.dirname( os.path.abspath(__file__))
     return open(
