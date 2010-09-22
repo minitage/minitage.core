2600ae8fff5d9b24c86841cab277a98502d84796
kiorky <kiorky@cryptelium.net>
2010-09-22 10:09:57 +0000

    remove unused entry points

--- setup.py
+++ setup.py
@@ -79,9 +79,6 @@ setup(
     entry_points = {
         'console_scripts': [
             'minimerge = minitage.core.launchers.minimerge:launch',
-            'apty = minitage.core.launchers.minimerge:launch',
-            'pacmerge = minitage.core.launchers.minimerge:launch',
-            'yasty = minitage.core.launchers.minimerge:launch',
         ],
     }
 
