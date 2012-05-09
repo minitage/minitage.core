34f6207fc2083b1e985d16420c7e9bd8a1c6732c
kiorky <kiorky@cryptelium.net>
2012-05-09 20:59:36 +0200

    Back to development: 2.0.34

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ import sys
 from setuptools import setup, find_packages
 
 name = 'minitage.core'
-version = '2.0.33'
+version = '2.0.34.dev0'
 def read(rnames):
     setupdir =  os.path.dirname( os.path.abspath(__file__))
     return open(
