66a9c9c4bda7f35b61efbb9be480f66bc2c1a12e
kiorky <kiorky@cryptelium.net>
2013-03-28 23:02:49 +0100

    offline, minitaigy, exit code, buildout maker

--- src/minitage/core/makers/buildout.py
+++ src/minitage/core/makers/buildout.py
@@ -38,6 +38,7 @@ import urllib2
 from minitage.core.makers  import interfaces
 import minitage.core.core
 import minitage.core.common
+import traceback
 
 from iniparse import ConfigParser
 
@@ -45,6 +46,47 @@ class BuildoutError(interfaces.IMakerError):
     """General Buildout Error."""
 
 __logger__ = 'minitage.makers.buildout'
+
+def select_ds(distribute_setup_places):
+    ds = None
+    for i in distribute_setup_places:
+        ds = os.path.join(i, 'distribute_setup.py')
+        if os.path.exists(ds):
+            break
+        else:
+            ds = None
+    return ds
+
+def select_fl(fl):
+    found = False
+    ret = None
+    for d in fl:
+        if os.path.exists(d):
+            files = os.listdir(d)
+            has_ds = True in [True
+             for a in files
+             if (a.startswith('distribute')
+                 and (a.endswith('gz')
+                     or a.endswith('zip')
+                 ))]
+            has_buildout = True in [True
+             for a in files
+             if (a.startswith('zc.buildout')
+                 and (a.endswith('gz')
+                     or a.endswith('zip')
+                 ))]
+            if has_buildout and has_ds:
+                ret = d
+                found = True
+                break
+    return ret
+
+def get_offline(opts):
+    offline = (
+        opts.get('offline', False)
+        or opts['minimerge']._offline)
+    return offline
+
 
 class BuildoutMaker(interfaces.IMaker):
     """Buildout Maker.
@@ -68,6 +110,57 @@ class BuildoutMaker(interfaces.IMaker):
         if switch == 'buildout':
             return True
         return False
+
+    def upgrade_bootstrap(self, minimerge, offline, directory="."):
+        buildout1 = False
+        try:
+            def findcfgs(path, cfgs=None):
+                if not cfgs: cfgs=[]
+                for p, ids, ifs in os.walk(path):
+                    for i in ifs:
+                        if i.endswith('.cfg'):
+                            cfgs.append(os.path.join(p, i))
+                return cfgs
+            files = findcfgs(directory)
+            for f in files:
+                fic = open(f)
+                if 'buildout.dumppick' in fic.read():
+                    buildout1 = True
+                fic.close()
+        except Exception, e:
+            pass
+        if buildout1:
+            booturl = 'http://downloads.buildout.org/1/bootstrap.py'
+        else:
+            booturl = 'http://downloads.buildout.org/2/bootstrap.py'
+        # try to donwload an uptodate bootstrap
+        if not offline:
+            try:
+                try:
+                    fic = open('bootstrap.py')
+                    oldcontent = fic.read()
+                    fic.close()
+                except:
+                    oldcontent = ""
+                try:
+                    open(
+                        os.path.join(minimerge.history_dir,
+                                     'updated_bootstrap'))
+                except:
+                    fic = open('bootstrap.py', 'w')
+                    data = urllib2.urlopen(booturl).read()
+                    fic.write(data)
+                    fic.close()
+                    self.logger.info('Bootstrap updated')
+                    fic = open(os.path.join(
+                        minimerge, 'updated_bootstrap'), 'w')
+                    fic.write('foo')
+                    fic.close()
+            except:
+                if oldcontent:
+                    fic = open('bootstrap.py', 'w')
+                    fic.write(oldcontent)
+                    fic.close() 
 
     def reinstall(self, directory, opts=None):
         """Rebuild a package.
@@ -105,10 +198,48 @@ class BuildoutMaker(interfaces.IMaker):
                                              self.buildout_config))
         cwd = os.getcwd()
         os.chdir(directory)
+        minibuild = opts.get('minibuild', None)
+        installed_cfg = os.path.join(directory, '.installed.cfg')
+        if not opts:
+            opts = {}
+        try:
+            parts = opts.get('parts', False)
+            if isinstance(parts, str):
+                parts = parts.split()
+            category = ''
+            if minibuild: category = minibuild.category
+            # Try to upgrade only if we need to
+            # (we chech only when we have a .installed.cfg file
+            if (not opts.get('upgrade', True)
+                and os.path.exists(installed_cfg)
+                and (not category=='eggs')):
+                self.logger.info(
+                    'Buildout will not run in %s'
+                    ' as there is a .installed.cfg file'
+                    ' indicating us that the software is already'
+                    ' installed but minimerge is running in'
+                    ' no-update mode. If you want to try'
+                    ' to update/rebuild it unconditionnaly,'
+                    ' please relaunch with -uUR.' % directory)
+                return
+            self.buildout_bootstrap(directory, opts)
+            self.buildout(directory, parts, opts)
+        except Exception, instance:
+            trace = traceback.format_exc()
+            os.chdir(cwd)
+            raise BuildoutError(
+                'Buildout failed:\n\t%s' % trace)
+        os.chdir(cwd)
+
+    def buildout_bootstrap(self, directory, opts):
+        offline = get_offline(opts)
         dcfg = os.path.expanduser('~/.buildout/default.cfg')
+        minimerge = opts.get('minimerge', None)
+        py = self.choose_python(directory, opts)
         downloads_caches = [
             os.path.abspath('../../downloads/dist'),
             os.path.abspath('../../downloads/minitage/eggs'),
+            os.path.abspath('../../downloads/minitage'),
             os.path.abspath('../../download/dist'),
             os.path.abspath('../../download/minitage/eggs'),
         ]
@@ -122,151 +253,128 @@ class BuildoutMaker(interfaces.IMaker):
                         places = [k,
                                   '%s/dist' % k,
                                   '%s/minitage/eggs'%k]
-                        downloads_caches.extend(places)
+                        for k in places:
+                            if not k in downloads_caches:
+                                downloads_caches.append(k)
             except Exception, e:
                 pass
         find_links = []
-        cache = os.path.abspath('../../eggs/cache')
+        cache = os.path.abspath(
+            os.path.join(directory, ('../../eggs/cache'))
+        )
         for c in [cache] + downloads_caches:
             if os.path.exists(c) and not c in find_links:
                 find_links.append(c)
+        distribute_setup_places = find_links[:]
+        bootstrap_args = ''
+        self.upgrade_bootstrap(minimerge, offline)
+        # be sure which buildout bootstrap we have
+        fic = open('bootstrap.py')
+        content = fic.read()
+        fic.close()
+        has_buildout2, has_buildout1 = False, False
+        if '--distribute' in content:
+            self.logger.warning('Using distribute !')
+            bootstrap_args += ' %s' % '--distribute'
+        if offline:
+            bootstrap_args += ' -t'
+        if ('--setup-source' in content 
+            and not "--find-links" in content):
+            ds = select_ds(distribute_setup_places)
+            if not ds and offline:
+                raise Exception(
+                    'Bootstrap failed, '
+                    'no distribute_setup.py '
+                    'found in %s '
+                    '' % ' '.join(
+                            distribute_setup_places))
+            if ds:
+                bootstrap_args += ' --setup-source %s' % ds
+                bootstrap_args += ' --eggs %s' % (
+                    os.path.abspath('../../eggs/cache'),
+                )
+        try:
+            eggs_base = select_fl(find_links)
+        except:
+            eggs_base = None
+            if offline:
+                raise Exception(
+                    'Missing either '
+                    'zc.buildout or distribute source')
+        if eggs_base is not None:
+            arg = ""
+            if '--download-base' in content:
+                arg = "--download-base"
+            if '--find-links' in content:
+                arg = "--find-links"
+            if arg:
+                bootstrap_args += ' %s "%s"' % (
+                    arg, eggs_base)
+        minitage.core.common.Popen(
+            '%s bootstrap.py %s ' % (py, bootstrap_args,),
+            opts.get('verbose', False)
+        )
+
+    def buildout(self,
+                 directory=".",
+                 parts=None,
+                 opts=None):
+        offline = get_offline(opts)
         bcmd = os.path.normpath('./bin/buildout')
         minibuild = opts.get('minibuild', None)
-        dependency_or_egg = getattr(minibuild, 'category', None) in ['dependencies', 'eggs']
+        dependency_or_egg = (getattr(minibuild, 'category', None) 
+                             in ['dependencies', 'eggs'])
+        offline = (opts.get('offline', False)
+                   or opts['minimerge']._offline)
+        argv = []
+        if not parts:
+            parts = []
         if not opts:
             opts = {}
-        try:
-            argv = []
-            if opts.get('verbose', False):
-                self.logger.debug('Buildout is running in verbose mode!')
-                argv.append('-vvvvvvv')
-            installed_cfg = os.path.join(directory, '.installed.cfg')
-            if (not opts.get('upgrade', True)
-                and not dependency_or_egg
-                and not os.path.exists(installed_cfg)):
-                argv.append('-N')
-            if opts.get('upgrade', False) or dependency_or_egg:
-                self.logger.debug('Buildout is running in newest mode!')
-                argv.append('-n')
-            if opts.get('offline', False):
-                self.logger.debug('Buildout is running in offline mode!')
-                argv.append('-o')
-            if opts.get('debug', False):
-                self.logger.debug('Buildout is running in debug mode!')
-                argv.append('-D')
-            parts = opts.get('parts', False)
-            if isinstance(parts, str):
-                parts = parts.split()
-            category = ''
-            if minibuild: category = minibuild.category
-            # Try to upgrade only if we need to
-            # (we chech only when we have a .installed.cfg file
-            if not opts.get('upgrade', True)\
-               and os.path.exists(installed_cfg) and (not category=='eggs'):
-                self.logger.info('Buildout will not run in %s'
-                                  ' as there is a .installed.cfg file'
-                                  ' indicating us that the software is already'
-                                  ' installed but minimerge is running in'
-                                  ' no-update mode. If you want to try'
-                                  ' to update/rebuild it unconditionnaly,'
-                                  ' please relaunch with -uUR.' % directory)
-                return
-
-
-            # running buildout in our internal way
-            # always regenerating that buildout file
-            #if not os.path.exists(
-            #    os.path.join(
-            #        directory,
-            #        'bin',
-            #        'buildout')):
-            bootstrap_args = ''
-            py = self.choose_python(directory, opts)
-            if os.path.exists('bootstrap.py'):
-                # if this bootstrap.py supports distribute, just use it!
-                content = open('bootstrap.py').read()
-                # special handly plone case
-                buildout1 = False
-                try:
-                    def findcfgs(path, cfgs=None):
-                        if not cfgs: cfgs=[]
-                        for p, ids, ifs in os.walk(path):
-                            for i in ifs:
-                                if i.endswith('.cfg'):
-                                    cfgs.append(os.path.join(p, i))
-                        return cfgs
-                    files = findcfgs('.')
-                    for f in files:
-                        fic = open(f)
-                        if 'buildout.dumppick' in fic.read():
-                            buildout1 = True
-                        fic.close()
-                except Exception, e:
-                    pass
-                if buildout1:
-                    booturl = 'http://downloads.buildout.org/1/bootstrap.py'
-                else:
-                    booturl = 'http://downloads.buildout.org/2/bootstrap.py'
-                # try to donwload an uptodate bootstrap
-                if not opts['minimerge']._offline:
-                    try:
-                        try:
-                            open(os.path.join(opts['minimerge'].history_dir, 'updated_bootstrap'))
-                        except:
-                            fic = open('bootstrap.py', 'w')
-                            data = urllib2.urlopen(booturl).read()
-                            fic.write(data)
-                            content = data
-                            fic.close()
-                            self.logger.info('Bootstrap updated')
-                            fic = open(os.path.join(opts['minimerge'].history_dir, 'updated_bootstrap'), 'w')
-                            fic.write('foo')
-                            fic.close()
-                    except:
-                        pass
-                if '--distribute' in content:
-                    self.logger.warning('Using distribute !')
-                    bootstrap_args += ' %s ' % '--distribute'
-                if opts['minimerge']._offline:
-                    bootstrap_args += ' -t '
-                if find_links:
-                    bootstrap_args += ''.join([' -f %s ' % c for c in find_links])
-                bootstrap_args += ' -c %s ' % self.buildout_config
+        if opts.get('verbose', False):
+            self.logger.debug(
+                'Buildout is running in verbose mode!')
+            argv.append('-vvvvvvv')
+        installed_cfg = os.path.join(directory, '.installed.cfg')
+        if (not opts.get('upgrade', True)
+            and not dependency_or_egg
+            and not os.path.exists(installed_cfg)):
+            argv.append('-N')
+        if opts.get('upgrade', False) or dependency_or_egg:
+            self.logger.debug(
+                'Buildout is running in newest mode!')
+            argv.append('-n')
+        if offline:
+            self.logger.debug(
+                'Buildout is running in offline mode!')
+            argv.append('-o')
+        if opts.get('debug', False):
+            self.logger.debug(
+                'Buildout is running in debug mode!')
+            argv.append('-D')
+        if parts:
+            for part in parts:
+                self.logger.info(
+                    'Installing single part: %s' % part)
                 minitage.core.common.Popen(
-                    '%s bootstrap.py %s ' % (py, bootstrap_args,),
+                    '%s -c %s %s install %s ' % (
+                        bcmd,
+                        self.buildout_config,
+                        ' '.join(argv),
+                        part
+                    ),
                     opts.get('verbose', False)
                 )
-            else:
-                minitage.core.common.Popen(
-                    'buildout bootstrap -c %s' % self.buildout_config,
-                    opts.get('verbose', False)
-                )
-            if parts:
-                for part in parts:
-                    self.logger.info('Installing single part: %s' % part)
-                    minitage.core.common.Popen(
-                        '%s -c %s %s install %s ' % (
-                            bcmd,
-                            self.buildout_config,
-                            ' '.join(argv),
-                            part
-                        ),
-                        opts.get('verbose', False)
-                    )
-            else:
-                self.logger.debug('Installing parts')
-                cmd = '%s -c %s %s ' % (
-                    bcmd,
-                    self.buildout_config,
-                    ' '.join(argv))
-                minitage.core.common.Popen(
-                    cmd,
-                    opts.get('verbose', False)
-                )
-        except Exception, instance:
-            os.chdir(cwd)
-            raise BuildoutError('Buildout failed:\n\t%s' % instance)
-        os.chdir(cwd)
+        else:
+            self.logger.debug('Installing parts')
+            cmd = '%s -c %s %s ' % (
+                bcmd,
+                self.buildout_config,
+                ' '.join(argv))
+            minitage.core.common.Popen(
+                cmd,
+                opts.get('verbose', False)
+            )
 
     def choose_python(self, directory, opts):
         python = sys.executable
